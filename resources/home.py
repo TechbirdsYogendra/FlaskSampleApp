43b33c6505cff8e1b5df6028234d3f1c237611c2
Yogendra Singh <yogsingh6@publicisgroupe.net>
2019-10-23 16:05:19 +0530

    Added render_template support

--- resources/home.py
+++ resources/home.py
@@ -4,6 +4,5 @@ from flask import render_template, make_response
 
 class Home(Resource):
     def get(self):
-       # return "Welcome "
         headers = {'Content-Type': 'text/html'}
         return make_response(render_template('home.html'), 200, headers)
