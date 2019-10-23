3b0d730d7b3efe977d54e6588c775b6bf91930da
Yogendra Singh <yogsingh6@publicisgroupe.net>
2019-10-23 15:09:41 +0530

    Added render_template support

--- resources/home.py
+++ resources/home.py
@@ -1,9 +1,9 @@
 from flask_restful import Resource
-#from flask import render_template
+from flask import render_template
 
 
 
 class Home(Resource):
     def get(self):
-        return "Welcome to Flask Application"
-        # render_template('home.html')
+        # return "Welcome to Flask Application"
+        return render_template('home.html')
